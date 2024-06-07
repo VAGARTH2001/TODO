6089d51c479aedae1bca301178bdbded9c5fff55
iam_vagarth <Vagarthd@gmail.com>
2024-06-07 20:32:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+import './App.css'
+
+function App() {
+ 
+ 
+
+  return (
+    <>
+    <Navbar/>
+    <div className='bg-yellow-300 h-[549px]'>
+     
+    </div>
+      
+    </>
+  )
+}
+
+export default App
